a2c759643ed7cd09f35657ee5cb2a7ab1b62b841
filipemmr <112185872+Filipemrr@users.noreply.github.com>
2023-05-03 19:03:12 -0300

    Questao do Baralho

--- stack/baralho.c
+++ stack/baralho.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 typedef struct Node {
     int data;
@@ -30,19 +31,21 @@ int pop(Stack* stack) {
     return data;
 }
 
-int isEmpty(Stack* stack) {
+int is_empty(Stack* stack) {
     if(stack->top == NULL)
         return 1;
     else
         return 0;
 }
 
+
+
 int batalha(Stack *jogador_A, Stack *jogador_B){
     int carta_A;
     int Carta_B;
     carta_A = pop(jogador_A); 
     Carta_B = pop(jogador_B);
-
+    printf("Carta A %d X Carta B %d\n", carta_A,Carta_B);
     if(carta_A > Carta_B){
         push(jogador_A,carta_A);
         return 1;
@@ -55,20 +58,65 @@ int batalha(Stack *jogador_A, Stack *jogador_B){
         return 0;
 }
 
+void embaralhar_pilha(Stack* stack) {
+    // Caso a pilha tenha menos de dois elementos, não há o que embaralhar
+    if (stack->top == NULL || stack->top->next == NULL) {
+        return;
+    }
+    
+    // Conta o número de elementos na pilha
+    int count = 0;
+    Node* node = stack->top;
+    while (node != NULL) {
+        count++;
+        node = node->next;
+    }
+    
+    // Percorre a pilha e troca cada nó com outro nó aleatório
+    node = stack->top;
+    for (int i = 0; i < count; i++) {
+        // Encontra um nó aleatório
+        int random_index = rand() % count;
+        Node* random_node = stack->top;
+        for (int j = 0; j < random_index; j++) {
+            random_node = random_node->next;
+        }
+        
+        // Troca os dados dos nós
+        int temp = node->data;
+        node->data = random_node->data;
+        random_node->data = temp;
+        
+        // Avança para o próximo nó
+        node = node->next;
+    }
+}
+
+void imprimir(Stack *principal){
+    Node *novo = principal -> top;
+    if(novo != NULL){
+        while(novo != NULL){
+            printf("%d\n", novo->data);
+            novo = novo->next;
+        }
+    }
+}
+
 
 int main(){
-    int n; //numero total de cartas  
-    scanf("%d",&n);//leio o numero total de cartas 
+    srand(time(NULL));
+    int numero_de_Cartas = rand() % 21;//leio o numero total de cartas 
+    printf("o tamanho do baralho na partida foi: %d\n", numero_de_Cartas);
+
 
     Stack *baralho;
     baralho = (Stack*)malloc(sizeof(Stack));
     baralho->top = NULL;
 
     //criando o baralho
-    for(int i = 0; i < n; i++){
-        int numero_carta;
-        scanf("%d", numero_carta); 
-        push(baralho,numero_carta);
+    for (int i = 0; i < numero_de_Cartas; i++) {
+        int carta = rand() % 51; // gera um número aleatório de 0 a 100
+        push(baralho,carta);
     }
 
     //CRIANDO as duas pilhas de jogadores no qual vao estar seus baralhos.
@@ -78,7 +126,7 @@ int main(){
     jogador_B = (Stack*)malloc(sizeof(Stack));
     jogador_A->top = NULL; 
     jogador_B->top = NULL;
-
+    //destribui as cartas
     while(1){
         if(is_empty(baralho))
             break;
@@ -86,16 +134,28 @@ int main(){
         push(jogador_A,pop(baralho));
         push(jogador_B,pop(baralho));
     }
+    
+    embaralhar_pilha(jogador_A);
+    printf("cartas do jogador A:\n");
+    imprimir(jogador_A);
 
+    embaralhar_pilha(jogador_B);
+    printf("cartas do jogador B:\n");
+    imprimir(jogador_B);
+    
     while(1)
     {
         if(is_empty(jogador_A) || is_empty(jogador_B)){
             if(is_empty(jogador_A) && is_empty(jogador_B) == 0){
-                printf("O jogador B ganhou");
+                printf("O jogador B ganhou\n");
+                printf("Seu baralho final foi:");
+                imprimir(jogador_B);
                 break;
             }
             else if(is_empty(jogador_B) && is_empty(jogador_A) == 0){
-                printf("O jogador A ganhou");
+                printf("O jogador A ganhou\n");
+                printf("Seu baralho final foi:\n");
+                imprimir(jogador_A);
                 break;
             }
             else{
@@ -105,16 +165,9 @@ int main(){
         }
 
         batalha(jogador_A,jogador_B);
-        embaralha_pilha(jogador_A);
-        embaralha_pilha(jogador_B);
-
-        
+        embaralhar_pilha(jogador_A);
+        embaralhar_pilha(jogador_B);
     }
-    
-
-
-
-
 
     return 0;
 }
