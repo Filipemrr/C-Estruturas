b62e9d22e5f9c09739aa7946eafdb563af7e8634
filipemmr <112185872+Filipemrr@users.noreply.github.com>
2023-05-24 23:31:33 -0300

    elevadores (95%)

--- elevadores.c
+++ elevadores.c
@@ -10,6 +10,7 @@ int andar_cima = 0;
 int coluna_cima = 0;
 int andar_baixo = 0;
 int coluna_baixo = 0;
+int retorno;
 int elevador_mais_proximo_andar, elevador_mais_proximo_coluna;
 char *matriz[LINHAS][COLUNAS];
 
@@ -24,14 +25,21 @@ int achar_mais_proximo(int andar, int porta)
         porta = 0;
         for (int w = 0; w < 3; w++)
         {
-            if (matriz[andar + i][w] != "[ ]")
-            {
+            //achou
+            if (matriz[andar + i][w] != "[ ]"){
                 flag_up = 1;
+
+                if(matriz[andar + i][porta] != "[ ]")
+                    coluna_cima = porta;
+                else
+                    coluna_cima = w;
+    
                 andar_cima = andar + i;
-                coluna_cima = w;
+
                 break;
             }
-            matriz[andar + i][w] = "[\033[32m⊠\033[0m]";
+
+            //matriz[andar + i][w] = "[\033[32m⊠\033[0m]";
             if (andar + i >= LINHAS)
             {
                 flag_up = 0;
@@ -47,13 +55,20 @@ int achar_mais_proximo(int andar, int porta)
     for (int i = 1; i < 300; i++){
         porta = 1;
         for (int w = 0; w < 3; w++){
+
+            //achou
             if (matriz[andar - i][w] != "[ ]"){
                 flag_down = 1;
                 andar_baixo = andar - i;
-                coluna_baixo = w;
+
+                if(matriz[andar + i][porta] != "[ ]")
+                    coluna_baixo = porta;
+                else
+                    coluna_baixo = w;
+
                 break;
             }
-            matriz[andar - i][w] = "[\033[32m⊠\033[0m]";
+            //matriz[andar - i][w] = "[\033[32m⊠\033[0m]";
             if (andar - i <= 0){
                 printf("\nNenhuma elevador acima");
                 break;
@@ -71,16 +86,18 @@ int achar_mais_proximo(int andar, int porta)
     elevador_cima = (andar_cima - andar) + (coluna_cima - porta);
     elevador_baixo = (andar - andar_baixo) + (porta - coluna_baixo);
 
+    //varredura de cima eh mais proxima
     if(elevador_cima < elevador_baixo)
         return 1;
+    //varredura de baixo eh mais proxima
     else if (elevador_baixo < elevador_cima)
         return 0;
-    else
+    else//ambas tem mesma distancia
         return 2;
     
 }
 
-    void limpar_matriz()
+void limpar_matriz()
     {
         for (int i = 0; i < LINHAS; i++)
         {
@@ -94,7 +111,7 @@ int achar_mais_proximo(int andar, int porta)
         }
     }
 
-    void printar_predio()
+void printar_predio()
     {
         for (int i = LINHAS - 1; i >= 0; i--)
         {
@@ -120,55 +137,92 @@ int achar_mais_proximo(int andar, int porta)
         }
     }
 
-    int main()
-    {
-        // Inicializando a semente do gerador de números aleatórios
-        srand(time(0));
+void tracar_caminho(int andar1, int coluna1, int andar2, int coluna2)
+{
+    int indice_andar = andar1;
+    int indice_coluna = coluna1;
 
-        for (int i = 0; i < LINHAS; i++)
+    int delta_andar = (andar2 > andar1) ? 1 : -1;
+    int delta_coluna = (coluna2 > coluna1) ? 1 : -1;
+
+    while (indice_andar != andar2 || indice_coluna != coluna2)
+    {
+        if (indice_andar != andar1 || indice_coluna != coluna1)
         {
-            for (int j = 0; j < COLUNAS; j++)
-            {
-                matriz[i][j] = "[ ]";
-            }
+            matriz[indice_andar][indice_coluna] = "[\033[32m⊠\033[0m]";
         }
 
-        // Adicionando [E] aleatoriamente em cada coluna
+        if (indice_coluna != coluna2)
+        {
+            indice_coluna += delta_coluna;
+        }
+        else if (indice_andar != andar2)
+        {
+            indice_andar += delta_andar;
+        }
+    }
+}
+
+
+
+int main()
+{
+    // Inicializando a semente do gerador de números aleatórios
+    srand(time(0));
+
+    for (int i = 0; i < LINHAS; i++)
+    {
         for (int j = 0; j < COLUNAS; j++)
         {
-            for (int k = 0; k < 5; k++)
-            {
-                int random_row = rand() % LINHAS;
-                matriz[random_row][j] = "[\033[31m⊠\033[0m]";
-            }
+            matriz[i][j] = "[ ]";
         }
-        printf("\n");
-        printar_predio();
-        printf("     2   1   0\n\n");
-
-        while (1)
-        {
-            int andar, porta, escolha;
-            printf("Quer pedir um elevador? Aperte 1\n");
-            printf("Se já chegou onde quer, aperte 2\n");
-            printf("Faça uma escolha: ");
-            scanf("%d", &escolha);
-
-            if (escolha == 1)
-            {
-
-                andar = rand() % LINHAS;
-                porta = rand() % COLUNAS;
-
-                matriz[andar][porta] = "[\033[93m⊠\033[0m]";
-                achar_mais_proximo(andar, porta);
-            }
-            
-            printar_predio();
-            limpar_matriz();
-
-            // usleep(1000000);  // Pausa de 1 segundo
-        }
-
-        return 0;
     }
+
+    // Adicionando [E] aleatoriamente em cada coluna
+    for (int j = 0; j < COLUNAS; j++)
+    {
+        for (int k = 0; k < 5; k++)
+        {
+            int random_row = rand() % LINHAS;
+            matriz[random_row][j] = "[\033[31m⊠\033[0m]";
+        }
+    }
+    printf("\n");
+    printar_predio();
+    printf("     2   1   0\n\n");
+
+    while (1)
+    {
+        int andar, porta, escolha;
+
+        printf("Quer pedir um elevador? Aperte 1\n");
+        printf("Se já chegou onde quer, aperte 2\n");
+        printf("Faça uma escolha: ");
+        scanf("%d", &escolha);
+
+        if (escolha == 1)
+        {
+            andar = rand() % LINHAS;
+            porta = rand() % COLUNAS;
+
+            matriz[andar][porta] = "[\033[93m⊠\033[0m]";
+            retorno = achar_mais_proximo(andar, porta);
+        }
+        if (escolha == 2)
+        {
+            if (retorno == 1 || retorno == 2)
+                tracar_caminho(andar, porta, andar_cima, coluna_cima);
+
+            if (retorno == 0)
+                tracar_caminho(andar, porta, andar_baixo, coluna_baixo);
+        }
+        if (escolha == 3)
+        {
+            printar_predio();
+        }
+
+        // usleep(1000000);  // Pausa de 1 segundo
+    }
+
+    return 0;
+}
